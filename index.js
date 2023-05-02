180230b7b7e5f4375afa9ed070f655d36e879f5a
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-05-03 00:21:09 +0600

    fixed untracked files

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 
 const app = express()
-
+const chefs = require('./data/chefInfo.json');
 const port = process.env.PORT ||5000;
+// app.get('/',(req,res) =>{
+//     res.send('news going on the way the way too ')
+// });
+
+
 app.get('/',(req,res) =>{
-    res.send('news going on the way ')
+    res.send(chefs)
 });
 
 app.listen(port,()=>{
